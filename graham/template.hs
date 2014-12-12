942ee6f0689d4260b005810cbd42fe5f034e6e0a
Erkin Unlu <erkin.unlu@monitise.com>
2014-12-12 09:50:15 +0200

    template done

--- graham/template.hs
+++ graham/template.hs
@@ -11,14 +11,18 @@ import Data.Char
 -- ===================================
 
 triangle :: Integer -> Integer
-triangle n = undefined
+triangle 0 = 0
+triangle n = n + triangle (n-1)
 
 -- ===================================
 -- Ex. 1
 -- ===================================
 
 count :: Eq a => a -> [a] -> Int
-count x xs = undefined
+count x [] = 0
+count x (a:as) 
+      | x == a = 1 + count x as
+      | otherwise = count x as
 
 xs = [1,2,35,2,3,4,8,2,9,0,5,2,8,4,9,1,9,7,3,9,2,0,5,2,7,6,92,8,3,6,1,9,2,4,8,7,1,2,8,0,4,5,2,3,6,2,3,9,8,4,7,1,4,0,1,8,4,1,2,4,56,7,2,98,3,5,28,4,0,12,4,6,8,1,9,4,8,62,3,71,0,3,8,10,2,4,7,12,9,0,3,47,1,0,23,4,8,1,20,5,7,29,3,5,68,23,5,6,3,4,98,1,0,2,3,8,1]
 ys = map (\x -> ((x + 1) * 3) ^ 3 - 7) xs
@@ -38,11 +42,20 @@ poem = [ "Three Types for the Lisp-kings under the parentheses,"
 -- ===================================
 
 euclid :: (Int,  Int) -> Int
-euclid (x, y) = undefined
+euclid (x, y) 
+       | x == y = x
+       | x > y = euclid (x-y, y)
+       | otherwise = euclid (y-x, x)
 
 -- ===================================
 -- Ex. 3
 -- ===================================
 
 funkyMap :: (a -> b) -> (a -> b) -> [a] -> [b]
-funkyMap f g xs = undefined
+funkyMap f g xs = funkInner f g xs 0
+
+funkInner f g [] pos = []
+funkInner f g (x:xs) pos 
+          | mod pos 2 == 0 = (f x) : funkInner f g xs (pos+1)
+          | otherwise = (g x) : funkInner f g xs (pos+1)
+          
